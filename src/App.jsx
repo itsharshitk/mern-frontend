20b75fbc881e0c0768ed6c6ec555e28fce2c347c
Harshit Katiyar <harshitknp0@gmail.com>
2026-07-01 02:15:34 +0530

    added dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Counter from "./components/Counter";
 import LoginForm from "./components/LoginForm";
 import Title from "./components/UseEffect";
@@ -6,12 +6,18 @@ import NotFound from "./components/NotFound";
 
 function App() {
   return(
-    <Routes>
-      <Route path="/" element={<Counter />} />
-      <Route path="/loginform" element={<LoginForm />} />
-      <Route path="/title" element={<Title />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Link to="/">Home</Link> |
+      <Link to="/loginform">LoginForm</Link> |
+      <Link to="/title/23">TestUseEffect</Link>
+
+      <Routes>
+        <Route path="/" element={<Counter />} />
+        <Route path="/loginform" element={<LoginForm />} />
+        <Route path="/title/:id" element={<Title />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -1,11 +1,19 @@
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Title(){
+    const { id } = useParams();
+
     useEffect(() => {
-        document.title = "TaskFlow"
-    }, []);
+        document.title = `TaskFlow ${id}`
+    }, [id]);
     
-    return <h1>Welcome to TaskFlow</h1>
+    return (
+        <>
+            <h1>Welcome to TaskFlow</h1>
+            <h3>The user's id is: {id}</h3>
+        </>
+    )
 }
 
-export default Title;
+export default Title
